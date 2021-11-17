55496793401adcda30936433e5a564ff5f04effb
root <hwl@ai.korea.ac.kr>
2021-11-17 16:24:11 +0000

    addding config-file, config-parser and dataloader

---
+++ utils/cfgParser.py
@@ -0,0 +1,30 @@
+
+
+def cfgParser(path):
+
+    f = open(path,'r')
+    #contents = list()
+    cfgDict = dict()
+    for i in f.readlines():
+        data = i.strip('\n')
+        if data == '' or data[0] == '#':
+            continue
+        name,value=data.split('=')
+
+        if name == 'lossFunction' or name == 'optimizer':
+            cfgDict[name] = value
+        elif name == 'dropOut':
+            cfgDict[name] = float(value)
+        elif name.find('Path') != -1:
+            cfgDict[name] = value
+        else:
+            cfgDict[name] = int(value)
+
+    return cfgDict
+
+
+def main():
+    cfgParser('../cfg/FASTSPEECH2_TRAIN.cfg')
+
+if __name__ == '__main__':
+    main()
